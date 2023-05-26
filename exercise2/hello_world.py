bb50ad4d960cb76ae538978ae97a15a437597f1f
Cory Martin <cory.r.martin@noaa.gov>
2023-05-26 14:05:33 -0500

    hello world scripts

---
+++ exercise2/hello_world.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# define some strings
+myname = "John Smith"
+myemail = "john.smith@noaa.gov"
+
+# print statement
+print(f"Hello world! My name is {myname} and my email address is {myemail}.")
---
+++ exercise2/hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# define some strings
+myname="John Smith"
+myemail="john.smith@noaa.gov"
+
+# echo statement
+echo "Hello world! My name is ${myname} and my email address is ${myemail}."
